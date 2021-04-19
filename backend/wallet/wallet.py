de743373d12f233e2a2cbfd43d07311e50f41e7d
Ta-SeenJunaid <taseen.junaid@gmail.com>
2021-04-19 09:21:46 +0600

    Serialized public key

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -4,7 +4,7 @@ import uuid
 from backend.config import STARTING_BALANCE
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
-from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.exceptions import InvalidSignature
 
 
@@ -16,22 +16,32 @@ class Wallet:
             ec.SECP256K1(),
             default_backend())
         self.public_key = self.private_key.public_key()
+        self.serialize_public_key()
 
     def sign(self, data):
         return self.private_key.sign(
             json.dumps(data).encode('utf-8'),
             ec.ECDSA(hashes.SHA256()))
 
+    def serialize_public_key(self):
+        self.public_key = self.public_key.public_bytes(
+            encoding = serialization.Encoding.PEM,
+            format = serialization.PublicFormat.SubjectPublicKeyInfo
+        ).decode('utf-8')
+
     @staticmethod
     def verify(public_key, data, signature):
+        deserialized_public_key = serialization.load_pem_public_key(
+            public_key.encode('utf-8'),
+            default_backend()
+        )
         try:
-            public_key.verify(signature,
+            deserialized_public_key.verify(signature,
                               json.dumps(data).encode('utf-8'),
                               ec.ECDSA(hashes.SHA256()))
             return True
         except InvalidSignature:
             return False
-
 
 
 def main():
@@ -51,6 +61,7 @@ def main():
 
     signature_should_be_invalid = Wallet.verify(Wallet().public_key, tempered_data, signature)
     print(f'signature_should_be_invalid: {signature_should_be_invalid}')
+
 
 if __name__ == '__main__':
     main()
