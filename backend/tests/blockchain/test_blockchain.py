3df21b0f3fe8cd7cad56806de205ddee07664bb9
Ta-SeenJunaid <taseen.junaid@gmail.com>
2021-06-12 20:00:07 +0600

    Test transaction chain validations

--- backend/tests/blockchain/test_blockchain.py
+++ backend/tests/blockchain/test_blockchain.py
@@ -1,6 +1,8 @@
 import pytest
 from backend.blockchain.blockchain import Blockchain
 from backend.blockchain.block import GENESIS_DATA
+from backend.wallet.transaction import Transaction
+from backend.wallet.wallet import Wallet
 
 
 def test_blockchain_instance():
@@ -21,7 +23,7 @@ def test_add_block():
 def blockchain_with_five_blocks():
     blockchain = Blockchain()
     for i in range(4):
-        blockchain.add_block(i)
+        blockchain.add_block([Transaction(Wallet(), 'recipient', i).to_json()])
     return blockchain
 
 
@@ -54,3 +56,36 @@ def test_replace_chain_with_tampered_chain(blockchain_with_five_blocks):
 
     with pytest.raises(Exception, match='The incoming chain is invalid'):
         blockchain.replace_chain(blockchain_with_five_blocks.chain)
+
+
+def test_valid_transaction_chain(blockchain_with_five_blocks):
+    Blockchain.is_valid_transaction_chain(blockchain_with_five_blocks.chain)
+
+
+def test_is_valid_transaction_chain_duplicate_transactions(blockchain_with_five_blocks):
+    transaction = Transaction(Wallet(), 'recipient', 1).to_json()
+    blockchain_with_five_blocks.add_block([transaction, transaction])
+
+    with pytest.raises(Exception, match='is not unique'):
+        Blockchain.is_valid_transaction_chain(blockchain_with_five_blocks.chain)
+
+
+def test_is_valid_transaction_chain_multiple_rewards(blockchain_with_five_blocks):
+    reward_1 = Transaction.reward_transaction(Wallet()).to_json()
+    reward_2 = Transaction.reward_transaction(Wallet()).to_json()
+    blockchain_with_five_blocks.add_block([reward_1, reward_2])
+
+    with pytest.raises(Exception, match='one mining reward per block'):
+        Blockchain.is_valid_transaction_chain(blockchain_with_five_blocks.chain)
+
+
+def test_is_valid_transaction_chain_bad_transaction(blockchain_with_five_blocks):
+    bad_transaction = Transaction(Wallet(), 'recipient', 1)
+    bad_transaction.input['signature'] = Wallet().sign(bad_transaction.output)
+    blockchain_with_five_blocks.add_block([bad_transaction.to_json()])
+
+    with pytest.raises(Exception):
+        Blockchain.is_valid_transaction_chain(blockchain_with_five_blocks.chain)
+
+
+
