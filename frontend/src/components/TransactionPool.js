c8cf8b2671ef05a6156ed7f8aded64445de29fef
Ta-SeenJunaid <taseen.junaid@gmail.com>
2022-01-09 14:17:00 +0100

    Mining

--- frontend/src/components/TransactionPool.js
+++ frontend/src/components/TransactionPool.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 import Transaction from './Transaction';
 import { API_BASE_URL, SECONDS_JS } from '../config';
+import history from '../history';
 
 const POOL_INTERVAL = 10 * SECONDS_JS
 
@@ -21,10 +23,19 @@ function TransactionPool() {
     useEffect(() => {
         fetchTransactions();
 
-        const intervalID = setInterval(fetchTransactions, POOL_INTERVAL);
+        const intervalId = setInterval(fetchTransactions, POOL_INTERVAL);
 
         return () => clearInterval(intervalId);
     }, []);
+
+    const fetchMineBlock = () => {
+        fetch(`${API_BASE_URL}/blockchain/mine`)
+        .then(() => {
+            alert('Success!')
+
+            history.push('/blockchain')
+        });
+    }
 
     return (
         <div className="TransactionPool">
@@ -41,6 +52,13 @@ function TransactionPool() {
                     ))
                 }
             </div>
+            <hr />
+            <Button
+                variant="danger"
+                onClick={fetchMineBlock}            
+            >
+                Mine a block of these transactions
+            </Button>
         </div>
     )
 }
